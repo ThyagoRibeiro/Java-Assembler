fd325abe6b0bd6bd59956db434e2553692e7beea
Thyago Ribeiro <thyagoribeirosantos@gmail.com>
2016-07-07 15:47:32 -0300

    Primeiro Commit

---
+++ OCD-EP2/src/EP2OCD.java
@@ -0,0 +1,36 @@
+public class EP2OCD {
+
+	public static void main(String[] args) {
+		
+		Memoria memoria = new Memoria();
+		
+		
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(InterfaceAssembler.class.getName()).log(java.util.logging.Level.SEVERE,
+					null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(InterfaceAssembler.class.getName()).log(java.util.logging.Level.SEVERE,
+					null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(InterfaceAssembler.class.getName()).log(java.util.logging.Level.SEVERE,
+					null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(InterfaceAssembler.class.getName()).log(java.util.logging.Level.SEVERE,
+					null, ex);
+		}
+
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new InterfaceAssembler().setVisible(true);
+			}
+		});
+	}
+
+}
